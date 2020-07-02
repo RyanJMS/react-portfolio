7fa5b189ac93331dee1a9299ce8e3480a1fd3591
RyanJMS <59546790+RyanJMS@users.noreply.github.com>
2020-07-01 22:28:48 -0400

    built navbar, pages, react-router, footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <span>&copy; Ryan Schock 2020</span>
+    </footer>
+  );
+}
---
+++ src/components/NavTabs/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+export default function NavTabs() {
+  const location = useLocation();
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link
+          to="/"
+          className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/portfolio"
+          className={
+            location.pathname === "/portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </Link>
+      </li>
+    </ul>
+  );
+}
